39a60312c7041bea35999c5fe8101bc8270131bd
med-redha <redhaboutahir@gmail.com>
2022-10-21 00:37:59 +0100

    refact: remove category filter component and place it in cateegory page

--- src/views/CategoryPage.vue
+++ src/views/CategoryPage.vue
@@ -9,73 +9,107 @@
           </div>
         </v-img>
       </div>
-
     </div>
     <div class="category_bread">
       <v-breadcrumbs divider=">"></v-breadcrumbs>
     </div>
     <div class="category_headContainer">Handbags</div>
-    <div class="category_section">
-      <div class="category_filterContainer"></div>
-      <div class="category_listContainer">
-        <div class="displayFilter">
-          <div class="df_lineOne">
+    <v-row>
+      <v-col cols="3" class="my-10">
+        <v-expansion-panels class="mb-6">
+          <v-expansion-panel v-for="(item, index) in lists" :key="index">
+            <v-expansion-panel-header
+              expand-icon="mdi-plus"
+              disable-icon-rotate
+            >
+              {{ item }}
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-checkbox
+                :label="checkbox"
+                v-for="(checkbox, index) in checkboxes"
+                :key="index"
+              ></v-checkbox>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+      <v-col cols="9">
+        <div class="category_listContainer">
+          <div class="displayFilter">
+            <div class="df_lineOne">
               <v-icon color="primary" large>mdi-apps-box</v-icon>
               <v-icon large class="mr-2">mdi-format-list-bulleted</v-icon>
               <div class="df_lineTxt">Showing 1 - 40 of 145 items</div>
-          </div>
-          <div class="df_lineTwo">
-              <div class="df_lineTxt">To Show : </div>
+            </div>
+            <div class="df_lineTwo">
+              <div class="df_lineTxt">To Show :</div>
               <div>
-              <input type="text" class="df_lineInput" placeholder="9">
+                <input type="text" class="df_lineInput" placeholder="9" />
               </div>
+            </div>
+            <div class="df_lineThree">
+              <div class="df_lineTxt">Sort By</div>
+              <v-select
+                :items="items"
+                filled
+                solo
+                background-color="#F1F1F1"
+                flat
+                label="Position"
+                class="mt-7"
+              >
+              </v-select>
+            </div>
           </div>
-          <div class="df_lineThree">
-            <div class="df_lineTxt">Sort By </div>
-            <v-select
-            :items="items"
-            filled
-            solo
-            background-color="#F1F1F1"
-            flat
-            label="Position"
-            class="mt-7"
-            ></v-select>
-          </div>
-      </div>
-        <v-row>
-          <v-col cols="4" v-for="(display, index) in categoryDis" :key="index" class="my-4">
-            <category-display :display="display" />
-
-          </v-col>
-        </v-row>
-        <div class="category_pagination" sub>
-          <div class="text-center">
-            <v-pagination v-model="page" :length="4"></v-pagination>
+          <v-row>
+            <v-col
+              cols="4"
+              v-for="(display, index) in categoryDis"
+              :key="index"
+              class="my-4"
+            >
+              <category-display :display="display" />
+            </v-col>
+          </v-row>
+          <div class="category_pagination" sub>
+            <div class="text-center">
+              <v-pagination v-model="page" :length="4"></v-pagination>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
-import CategoryDisplay from '../components/categoryDisplay.vue';
-import grand from '../assets/arrivals/bagOne.jpg';
-import remus from '../assets/arrivals/bagThree.jpg';
-import couach from '../assets/arrivals/bagTwo.jpg';
-import boujee from '../assets/arrivals/bagFour.jpg';
-
+import CategoryDisplay from "../components/categoryDisplay.vue";
+import grand from "../assets/arrivals/bagOne.jpg";
+import remus from "../assets/arrivals/bagThree.jpg";
+import couach from "../assets/arrivals/bagTwo.jpg";
+import boujee from "../assets/arrivals/bagFour.jpg";
 
 export default {
-  name: 'categoryPage',
+  name: "categoryPage",
   components: {
     CategoryDisplay,
   },
   data() {
     return {
       page: 1,
-      items: ['Position', ],
+      items: ["Position"],
+      lists: ["Size", "Color", "brand", "discount", "hey", "hello", "why"],
+
+      checkboxes: [
+        "Blue",
+        "Crimson red",
+        "Selena Park",
+        "Teal",
+        "Aquamarine",
+        "Off-white",
+        "Mauve Orange",
+      ],
       categoryDis: [
         {
           img: grand,
@@ -176,13 +210,11 @@ export default {
           oldPrice: "$78.66",
           discount: "50% OFF",
         },
-
       ],
-    }
-  }
-}
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
