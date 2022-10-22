f26864d2ba0125adf8ce7bab454b54c2dbca2dba
med-redha <redhaboutahir@gmail.com>
2022-10-22 22:42:06 +0100

    refact: separated the component into two diffrent components

--- src/components/myCartComponent.vue
+++ src/components/myCartComponent.vue
@@ -1,29 +1,23 @@
 <template>
-  <div class="myCartComponent">
+  <div class="myCartComponent" :comp="comp">
     <div class="mCc_sectionOne">
-        <div class="mCcImg_holder"><img src="mCc_img" alt=""></div>
-        <div class="mCcSecOne_txt">
-            <div class="mCc_title"></div>
-            <div class="mCc_subtitle"></div>
-            <div class="mCc_subhead"></div>
-        </div>
+      <div class="mCcImg_holder">
+        <img class="mCc_img" :src="comp.img" alt="" />
+      </div>
+      <div class="mCcSecOne_txt">
+        <div class="mCc_title">{{ comp.title }}</div>
+        <div class="mCc_subtitle">{{ comp.subtitle }}</div>
+        <div class="mCc_subhead">{{ comp.subhead }}</div>
+      </div>
     </div>
-    <div class="mCc_sectionTwo">
-        <div class="mCcst_txtGrp">
-            <div class="mCcst_title"></div>
-            <div class="mCcst_title"></div>
-            <div class="mCcst_title"></div>
-        </div>
-        <div class="mCcst_btnGrp">
-            <v-btn class="mCcst_btnOne">Move to Wishlist</v-btn>
-            <v-btn class="mCcst_btnTwo">Remove</v-btn>
-        </div>
-    </div>
+   
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    comp: Object,
+  },
+};
 </script>
