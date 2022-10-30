d6efdf05970a9da6e87e5f9f7cd2bbcca3186c7d
med-redha <redhaboutahir@gmail.com>
2022-10-30 20:16:07 +0100

    refact: restructuring the page and adding the necessary details

--- src/views/myCart.vue
+++ src/views/myCart.vue
@@ -4,8 +4,8 @@
     <div class="cart_name my-8">myCart</div>
     <div class="mC_container">
       <v-row>
-        <v-col cols="8">
-          <div class="mCsection_one">
+        <v-col cols="7">
+          <div class="mCsection_one mr-12">
             <div class="mCso_table">
               <div class="mCsoTitle_head">Product Name</div>
               <div class="midTitle_holder">
@@ -17,7 +17,7 @@
             <v-divider></v-divider>
 
             <div class="mCsoComponent_container my-8">
-              <div class="componentOne">
+              <div class="componentOne my-2">
                 <my-cart-component
                   v-for="(s, index) in cartComponent"
                   :key="index"
@@ -32,23 +32,41 @@
                 />
               </div>
             </div>
-            <div class="mCsoSub_header my-12"></div>
-            <div class="mCso_btnGrp">
-              <input class="mCso_input" type="text" />
-              <v-btn class="mCso_btn">gfdgdfg</v-btn>
+            <div class="mCsoSub_header mt-12 mb-2">
+              <v-select :items="items" label="Apply Copon Code" class="slct" light>
+                {{items}}
+              </v-select>
+            </div>
+            <div class="mCso_btnGrp mb-8 mt-4">
+              <input class="mCso_input pl-4" type="text"  placeholder="Apply Copon Code"/>
+              <v-btn class="mCso_btn" text>check</v-btn>
             </div>
           </div>
         </v-col>
-        <v-col cols="4">
-          <div class="mCsection_two">
-            <div class="mCst_header">Order Summary</div>
-            <div class="mCst_details">
-              <div class="mCst_detailsTxt"></div>
-              <div class="mCst_detailsNum"></div>
+        <v-col cols="5">
+          <div class="mCsection_two ml-4 mt-3">
+            <div class="mCst_header mb-6">
+              <div class="head_devide">
+                <div class="header_txt mb-2">Order Summary</div>
+                <v-divider></v-divider>
+              </div>
+
+            </div>
+            
+            <div class="mCst_details my-4" >
+              <div class="mCst_detailsContainer my-2" v-for="(txt,index) in sectionTwoTxt" :key="index">
+                <div class="mCst_detailsTxt"> {{txt.title}}</div>
+              <div class="mCst_detailsNum"> {{txt.content}}</div>
+              </div>
+              
+            </div>
+            <div class="Tot d-flex mb-6" v-for="(tot, i) in Total" :key="i">
+              <div class="tot_txt"> {{tot.title}}</div>
+              <div class="tot_num"> {{tot.content}}</div>
             </div>
             <div class="mCst_btnGrp">
-              <v-btn class="mCst_btnOne">cs</v-btn>
-              <v-btn class="mCst_btnTwo">Po</v-btn>
+              <v-btn class="mCst_btnOne mr-6" depressed><span class="btn_span">Place Order</span></v-btn>
+              <v-btn class="mCst_btnTwo " outlined><span class="btn_span">Continue Shopping</span> </v-btn>
             </div>
           </div>
         </v-col>
@@ -69,6 +87,30 @@ export default {
   },
   data() {
     return {
+       sectionTwoTxt:[
+        {
+          title: 'sub Total',
+          content: '$119.69',
+        },
+        {
+          title: 'Discount',
+          content: '-$13.40',
+        },
+        {
+          title: 'Delivery Fee',
+          content: '-$0.00',
+        },
+
+       ],
+       Total:[
+        {
+          title: 'Grand Total',
+          content: '$106.29',
+        },
+       ],
+      
+
+      items:["Apply Coupon Code"],
       cartComponent: [
         {
           img: cImg,
