02efd95001ea2e23cf2154eba514f8d003002503
med-redha <redhaboutahir@gmail.com>
2022-11-01 00:13:20 +0100

    refact: restructuring the page adding elements and classes with modification

--- src/views/userProfile.vue
+++ src/views/userProfile.vue
@@ -4,108 +4,134 @@
       <v-toolbar flat>
         <v-toolbar-title>User Profile</v-toolbar-title>
       </v-toolbar>
-      <v-tabs vertical background-color="#F1F1F1">
-        <v-tab>
+      <v-tabs vertical background-color="#F1F1F1" >
+        <v-tab class="mb-3">
           <span class="tabs_side">Personal Information</span>
           <v-spacer></v-spacer> <v-icon large>mdi-chevron-right</v-icon></v-tab
         >
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">refer and earn</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon></v-tab
         >
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">My Orders</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon>
         </v-tab>
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">My Wishlist</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon>
         </v-tab>
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">My Reviews</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon>
         </v-tab>
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">My Adress Book</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon>
         </v-tab>
-        <v-tab>
+        <v-tab class="mb-3">
           <span class="tabs_side">My Saved Cards</span> <v-spacer></v-spacer>
           <v-icon large>mdi-chevron-right</v-icon>
         </v-tab>
 
         <v-tab-item>
-          <div class="tab_items">
-            <div class="cards_Title">
-              <div></div>
-              <div></div>
-              <v-devider></v-devider>
+          <div class="tab_items ml-4 mb-12">
+            <div class="cards_Title mb-4">
+              <div class="title_holder mb-2">
+                <div class="title_txt">Personal Information</div>
+                <div>
+                  <v-btn outlined color="#1b4b66"
+                    ><v-icon small>mdi-logout</v-icon>
+                    <span>Logout</span></v-btn
+                  >
+                </div>
+              </div>
+
+              <div class="devider"></div>
             </div>
 
-            <div class="ProfileImg_holder">
-              <v-img class="profile_pic" src="" alt=""> </v-img>
-              <div class="ProfilePic_btnGrp">
-                <v-btn class="ProfilePic_btnOne"></v-btn>
-                <v-btn class="ProfilePic_btnTwo"></v-btn>
+            <div class="ProfileImg_holder my-6">
+              <v-img class="profile_pic" src="../assets/aboutOne.jpg" alt="">
+              </v-img>
+              <div class="ProfilePic_btnGrp ml-2">
+                <v-btn class="ProfilePic_btnOne" depressed color="#1b4b66"
+                  ><span class="imgBtn_txt">Upload</span></v-btn
+                >
+                <v-btn class="ProfilePic_btnTwo" outlined color="red"
+                  ><v-icon small>mdi-delete-</v-icon>
+                  <span class="imgBtn_txtTwo">Delete</span>
+                </v-btn>
               </div>
             </div>
-            <div class="infoSection">
+            <div class="infoSection my-4">
               <div class="partONe_container">
                 <div class="infoPart_one">
-                  <span>First Name</span>
-                  <input type="text" class="inp_One" placeholder="john" />
-                </div>
-                <div class="infoPart_one">
                   <span class="infoTxt">First Name</span>
+                  <input type="text" class="inp_One mr-10" placeholder="john" />
+                </div>
+                <div class="infoPart_one my-4">
+                  <span class="infoTxt mx-2">Last Name</span>
                   <input type="text" class="inp_One" placeholder="doe" />
                 </div>
               </div>
-              <div class="infoPart_one">
-                <span>Email</span>
-                <input
-                  type="text"
-                  class="inp_mail"
-                  placeholder="johnDoe@empl.com"
-                />
-              </div>
-              <div class="infoPart_one">
-                <span>Mobile Number</span>
-                <div>
-                  <input type="text" class="inp_ph" placeholder="+11" />
-                  <input type="text" class="inp_One" placeholder="john" />
+              <div class="">
+                <div class="infoPart_MfD my-2">
+                  <span class="infoTxt">Email</span>
+                  <input
+                    type="text"
+                    class="inp_mail"
+                    placeholder="johnDoe@empl.com"
+                  />
+                </div>
+                <div class="infoPart_MfD my-2">
+                  <span class="infoTxt">Mobile Number</span>
+                  <div>
+                    <input type="text" class="inp_ph mr-2" placeholder="+11" />
+                    <input type="text" class="inp_pht" placeholder="john" />
+                  </div>
+                </div>
+                <div class="infoPart_MfD my-2">
+                  <span class="infoTxt">Date Of Birth</span>
+                  <input type="text" class="inp_phd" placeholder="DD/MM/yyyy" />
                 </div>
               </div>
-              <div class="infoPart_one">
-                <span>Date Of Birth</span>
-                <input type="text" class="inp_One" placeholder="DD/MM/yyyy" />
-              </div>
-              <div class="PartTWo_container">
-                <div class="secnd_title">
-                  <span class="stitle_txt">Cange Password</span>
-                  <v-devider></v-devider>
+              <div class="PartTWo_container my-4">
+                <div class="secnd_title my-4">
+                  <span class="stitle_txt mb-4">Change Password</span>
+                  <div class="devider mt-2"></div>
                 </div>
-                <div class="infoPart_one">
-                  <span>current password</span>
-                  <input type="password" class="inp_One" placeholder="....." />
-                </div>
-                <div class="infoPart_one">
-                  <span>new password</span>
-                  <input type="password" class="inp_One" placeholder="....." />
-                  <v-icon small>mdi-eye-outline</v-icon>
-                </div>
-                <div class="infoPart_one">
-                  <span>confirm password</span>
+                <div class="infoPart_two my-2">
+                  <span class="infoTxt">current password</span>
                   <input
                     type="password"
-                    class="inp_One"
-                    placeholder=".........."
+                    class="inp_two"
+                    placeholder="current password"
+                  />
+                </div>
+                <div class="infoPart_twop my-2">
+                  <span class="infoTxt">new password</span>
+                  <input
+                    type="password"
+                    class="inp_two ml-8"
+                    placeholder="new password"
+                  />
+                  <v-btn depressed color="#f1f1f1" class="inp_icn" height="52px"
+                    ><v-icon small color="">mdi-eye-outline</v-icon></v-btn
+                  >
+                </div>
+                <div class="infoPart_two my-2">
+                  <span class="infoTxt">confirm password</span>
+                  <input
+                    type="password"
+                    class="inp_two"
+                    placeholder="confirm password"
                   />
                 </div>
               </div>
             </div>
-            <div class="save_holder">
-              <v-btn class="save_btn" depressed
-                ><span>Save Changes</span></v-btn
+            <div class="save_holder my-6">
+              <v-btn class="save_btn" depressed color="#1b4b66"
+                ><span class="save_btnTxt">Save Changes</span></v-btn
               >
             </div>
           </div>
