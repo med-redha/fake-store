f1ee87bbefcb8467be7ce500060ad23b8cff2dc9
med-redha <redhaboutahir@gmail.com>
2022-11-05 02:08:12 +0100

    refact: structure and tabs

---
+++ src/components/myOrders.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="myOrders" :oreders="oreders">
+    <v-toolbar flat color="primary" dark>
+      <v-toolbar-title>My Orders</v-toolbar-title>
+    </v-toolbar>
+    <v-tabs>
+      <v-tab v-for="(oreder, index) in oreders" :key="index">
+        {{ oreder.title }}
+      </v-tab>
+
+      <v-tab-item v-for="(oreder, index) in oreders" :key="index">
+        <v-data-table
+        hide-default-footer
+          :headers="oreder.headers"
+          :items="oreder.desserts"
+          item-key="name"
+          v-model="selected"
+          show-select
+        >
+          <template v-slot:[`item.actions`]="{item}">
+            <v-icon small class="mr-2" >
+              mdi-chevron-right
+              {{item}}
+            </v-icon>
+          </template>
+         
+        </v-data-table>
+      </v-tab-item>
+    </v-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "myOrders",
+  props: {
+    oreders: Array,
+  },
+  data(){
+    return{
+      selected: [],
+    }
+  }
+};
+</script>
+
+<style></style>
