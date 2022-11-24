7266a079dac496272c57b5ebe2775df6d2165e5b
med-redha <redhaboutahir@gmail.com>
2022-11-24 05:38:03 +0100

    feat: create the payment method page

---
+++ src/components/paymentCards.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="paymentCard" :pay="pay">
+    <div class="checkbox_holder">
+      <v-radio-group v-model="radios" >
+        <v-radio label="" value="radio-1" v-on:click="googlePlay"></v-radio>
+      </v-radio-group>
+    </div>
+    <div class="img_holder">
+      <v-img class="pay_img" :src="pay.img" alt=""> </v-img>
+    </div>
+    <span class="title_txt mt-1">{{ pay.title }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radios: null,
+    };
+  },
+  props: {
+    pay: Object,
+  },
+};
+</script>
+
+<style></style>
